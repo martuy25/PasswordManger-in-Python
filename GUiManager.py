c84248781a064c03bbc66d9a572f9f9acf9a5103
martuy25 <133281613+martuy25@users.noreply.github.com>
2023-09-08 14:33:27 -0500

    Update GUiManager.py
    This repository contains a Python-based Password Manager application, and this update introduces several new features, including a complex password generator. The Password Manager allows users to securely store and retrieve their account passwords while also generating strong, random passwords for new accounts.
    
    New Features
    1. Complex Password Generator
    A new "Generate Password" button has been added to the Password Manager interface. When clicked, this button generates strong and complex random passwords, which can be useful for enhancing the security of your accounts.
    
    2. Display Generated Password
    A label has been added beneath the "Generate Password" button to display the generated password. This provides users with immediate access to the randomly generated passwords for their accounts.
    
    3. Password Strength Checker
    We have added a robust password strength checker to help users create stronger passwords. This feature evaluates the complexity of entered passwords based on criteria such as length, the use of upper and lower case letters, numbers, and special characters. Users can now assess the strength of their passwords in real-time.
    
    4. Update and Delete Passwords
    Managing stored passwords has never been easier. With the new "Update Password" and "Delete Password" buttons, users can seamlessly update or remove existing passwords from their accounts. This feature adds a layer of flexibility and control to password management.

--- GUiManager.py
+++ GUiManager.py
@@ -2,10 +2,16 @@ import tkinter as tk
 from tkinter import messagebox
 from cryptography.fernet import Fernet
 import sqlite3
+import os
+import secrets
+import string
 
 #creating the main window
 window = tk.Tk()
 window.title("Password Manager")
+
+#Create golbal database
+conn = None
 
 #Creating labels, entry fields and buttons
 label_account = tk.Label(window, text="Account")
@@ -14,6 +20,12 @@ label_password = tk.Label(window, text="Password")
 entry_password = tk.Entry(window, show="*")
 button_save = tk.Button(window, text="Save Password")
 button_show = tk.Button(window, text="Show Password")
+button_generate_password = tk.Button(window, text="Generate Password")
+label_password_strength = tk.Label(window, text="Password Strength:")
+button_check_password = tk.Button(window, text="Check Password Strength")
+entry_check_password = tk.Entry(window, show="*")
+button_update_password = tk.Button(window, text="Update Password")
+button_delete_password = tk.Button(window, text="Delete Password")
 
 
 # Creating the Grid layout
@@ -23,10 +35,19 @@ label_password.grid(row=1, column=0)
 entry_password.grid(row=1, column=1)
 button_save.grid(row=2, column=0)
 button_show.grid(row=2, column=1)
+button_generate_password.grid(row=3, column=0, columnspan=2)
+label_password_strength.grid(row=5, column=0, columnspan=2)
+button_check_password.grid(row=7, column=0, columnspan=2)
+entry_check_password.grid(row=6, column=0, columnspan=2)
+button_update_password.grid(row=8, column=0)
+button_delete_password.grid(row=8, column=1)
+
+
 
 #Generate a key and storing securely
 #define key path
 key_file_path = "secret.key"
+
 
 #check to see if key file exist
 if os.path.isfile(key_file_path):
@@ -42,6 +63,7 @@ else:
 #cipher_key
 cipher_suite = Fernet(key)
 
+# Encrypt and decrypt functions
 def encrypt(password):
     encrypted_password = cipher_suite.encrypt(password.encode())
     return encrypted_password
@@ -50,20 +72,85 @@ def decrypt(encrypted_password):
     decrypted_password = cipher_suite.decrypt(encrypted_password).decode()
     return decrypted_password
 
+def generate_complex_password():
+    #defining character sets
+    letters = string.ascii_letters
+    num = string.digits
+    special_characters = string.punctuation
+    
+    complexPass = (secrets.choice(letters) + secrets.choice(num) + secrets.choice(special_characters) +
+        ''.join(secrets.choice(letters+num+special_characters) for i in range(12))
+    )    
+    entry_password.delete(0, tk.END)
+    entry_password.insert(0, complexPass)
+        
+def check_password_strength(password):
+    length_check = len(password) >= 8
+    uppercase_check = any(check.isupper() for check in password)
+    lowercase_check = any(check.islower() for check in password)
+    num_check = any(check.isdigit() for check in password)
+    special_check = any(check in string.punctuation for check in password)
+    
+   #calculate password strength
+    strength_score = sum([length_check, uppercase_check, lowercase_check, num_check, special_check])
+    # Display score
+    strength_labels = ["Very Weak", "Weak", "Moderate", "Strong", "Very Strong"]
+    password_strength = strength_labels[strength_score]
+    label_password_strength.config(text="Password Strength: " + password_strength)
+
 
 #creating the function to save the passwords
 def save_password():
+    global conn #to access the global database
     account = entry_account.get()
     password = entry_password.get()
     encrypt_password = encrypt(password)
-    conn = sqlite3.connect("passwords.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO passwords (account, password) VALUES(?, ?)", (account, encrypt_password))
-    conn.commit()
-    conn.close()
-    entry_account.delete(0, tk.END)
-    entry_password.delete(0, tk.END)
-    messagebox.showinfo("Success", "Password saved succesfully")
+    
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO passwords (account, password) VALUES(?, ?)", (account, encrypt_password))
+        entry_account.delete(0, tk.END)
+        entry_password.delete(0, tk.END)
+        messagebox.showinfo("Success", "Password saved successfully")
+    except sqlite3.IntegrityError:
+        messagebox.showerror("Error", "Account already exists in the database")
+   
+
+# Function to update a password
+def update_password():
+    global conn 
+    account = entry_account.get()
+    new_password = entry_password.get()
+    encrypt_new_password = encrypt(new_password)
+    
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE passwords SET password=? WHERE account=?", (encrypt_new_password, account))
+        entry_account.delete(0, tk.END)
+        entry_password.delete(0, tk.END)
+        messagebox.showinfo("Success", "Password updated successfully")
+    except sqlite3.IntegrityError:
+        messagebox.showerror("Error", "Account not found in the database")
+
+    
+
+# Function to delete a password
+def delete_password():
+    global conn
+    account = entry_account.get()
+    
+    try:
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM passwords WHERE account=?", (account,))
+        entry_account.delete(0, tk.END)
+        entry_password.delete(0, tk.END)
+        messagebox.showinfo("Success", "Password deleted successfully")
+    except sqlite3.IntegrityError:
+        messagebox.showerror("Error", "Account not found in the database")
+
     
 #Function to show passwords
 def show_password():
@@ -89,6 +176,24 @@ conn.close()
 # Bind functions to buttons
 button_save.config(command=save_password)
 button_show.config(command=show_password)
+button_generate_password.config(command=generate_complex_password)
+button_check_password.config(command=lambda: check_password_strength(entry_check_password.get()))
+button_update_password.config(command=update_password)
+button_delete_password.config(command=delete_password)
+
+security_explanation = """
+Security Note:
+The complex password generator uses the 'secrets' module, which is designed specifically for cryptographic applications and provides a higher level of security compared to the 'random' module. 'secrets' generates cryptographically secure random numbers suitable for password generation, making it extremely difficult for attackers to predict or guess generated passwords.
+
+Additionally, the generated password includes a mix of uppercase and lowercase letters, digits, and special characters, making it more resilient against common password attacks.
+
+Remember to store generated passwords securely and never reveal them unnecessarily. Consider using a password manager to manage and store your passwords safely.
+"""
+
+# Display the security explanation in a message box
+messagebox.showinfo("Security Information", security_explanation)
+
+conn = sqlite3.connect("passwords.db")
 
 # Start the GUI
 window.mainloop()
